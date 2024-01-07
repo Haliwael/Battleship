581f2d2abd0e6ba7e4a82f64a2da9c19bd2e7f3a
wael <waelhajali@gmail.com>
2024-01-07 18:42:35 +0100

    added UI + some functionalities

--- main.js
+++ main.js
@@ -67,13 +67,61 @@ class gameboard{
 
 }
 class player extends gameboard{}
-function placingShips(){
+
+function playersTurn(board){
     
 }
-function startGame(){
 
+function startGame(){
     const p1 = new player()
     const p2 = new player()
+    const item = document.querySelectorAll(".dragable")
+    const containers = document.querySelectorAll(".squares")
+    const grid = document.querySelector(".board-1")
+    const grid2 = document.querySelector(".board-2")
+    item.forEach((i) =>{
+        i.addEventListener("dragstart",()=>{
+            i.classList.add('dragging')
 
+        })
+        i.addEventListener("dragend",()=>{
+            i.classList.remove('dragging')
+        })
+    })
+    containers.forEach((container)=>{
+        container.addEventListener('dragover',(e)=>{
+            e.preventDefault()
+            const draggable = document.querySelector(".dragging")
+            
+            if(container.childElementCount == 0 ){
+                container.appendChild(draggable)
+            }
+            
+        })
+    })
+
+    grid.addEventListener("click",e =>{
+        let c = "";
+        if(e.target.draggable){
+            
+            c = e.target.parentNode.parentNode.children
+        }
+        else{
+            c = e.target.parentNode.children
+            
+        }
+        
+        for(let i =0;i<c.length;i++){
+            
+            if(c[i] === e.target.parentNode || c[i] === e.target){
+                console.log(`row = ${(Math.floor(i/5))} col = ${(i%5)}`);
+                break;
+            }
+            
+        }
+        
+
+    })
 
 }
+startGame()
