bc40c855f3afaa396e9df68bcaf561931e983625
wael <waelhajali16@gmail.com>
2024-01-13 13:47:57 +0100

    fixed some bugs + improved the ui

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-let gameStatus = 1
 const item = document.querySelectorAll(".dragable")
 const containers = document.querySelectorAll(".squares")
+const body = document.querySelector("body")
 const grid = document.querySelector(".board-1")
 const grid2 = document.querySelector(".board-2")
-const btn = document.querySelector("button")
+const btn = document.querySelector(".start")
+const pop = document.querySelector(".pop-up")
 const hit = '<span class="material-symbols-outlined" style="pointer-events: none; user-select:none;">close</span>';
+const gameOver = document.querySelector(".gameOver")
 let turn = 0;
 let isShipsPlaced = false;
+let gameStatus = 1
 class ship{
     constructor(x,y,length){
         this.length = length;
@@ -61,34 +64,62 @@ const p2 = new player()
 item.forEach((i) =>{
     i.addEventListener("dragstart",()=>{
         if(!isShipsPlaced){
-            i.classList.add('dragging')
+            if(i.parentNode.parentNode.classList.contains("player-1")){
+                i.classList.add('dragging-1')
+            }
+            if(i.parentNode.parentNode.classList.contains("player-2")){
+                i.classList.add('dragging-2')
+            }
+            
         }
         
 
     })
     i.addEventListener("dragend",()=>{
         if (!isShipsPlaced) {
-            i.classList.remove('dragging')
+            if(i.parentNode.parentNode.classList.contains("player-1")){
+                i.classList.remove('dragging-1')
+            }
+            if(i.parentNode.parentNode.classList.contains("player-2")){
+                i.classList.remove('dragging-2')
+            }
         }
     })
     })
 containers.forEach((container)=>{
         container.addEventListener('dragover',(e)=>{
             e.preventDefault()
-            const draggable = document.querySelector(".dragging")
+            let draggable = null;
             
             if(container.childElementCount == 0 && !isShipsPlaced ){
-                container.appendChild(draggable)
+                if( container.parentNode.classList.contains("board-1")){
+                    draggable = document.querySelector(".dragging-1")
+                    container.appendChild(draggable)
+                }
+                if( container.parentNode.classList.contains("board-2")){
+                    draggable = document.querySelector(".dragging-2")
+                    container.appendChild(draggable)
+                }
             }
+            
             
         })
     })
 
 //button to start the game 
 btn.addEventListener("click",()=>{
+
     placingShips(p1,grid)
     placingShips(p2,grid2)
-    startGame()
+    
+    if(p1.ships.length < 3 || p2.ships.length < 3){
+        alert("place all your ships")
+    }
+    else{
+        
+        startGame() 
+    }
+   
 })
 function board1(e){
     let c = "";
@@ -123,7 +154,9 @@ function board1(e){
             }
             if(p1.checkShips()){
                 gameStatus = 0
-               
+                gameOver.classList.add("active")
+                pop.innerHTML = `<p>Player 2 won</p>
+                <button onclick="location.reload()">Play again</button>`
                 
             }
             
@@ -166,7 +199,10 @@ function board2(e){
         }
         if(p2.checkShips()){
             gameStatus = 0;
-            return "player 1 won"
+            gameOver.classList.add("active")
+            pop.innerHTML = `<p>Player 1 won</p>
+            <button onclick="location.reload()">Play again</button>`
+                
         }
      
         
